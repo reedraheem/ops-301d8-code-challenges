4eee12b6086c5fe2b5965fdbde8049730b825c3a
reedraheem <Reedsharif@yahoo.com>
2023-06-03 22:50:48 +0000

    cc2

--- cc2.sh
+++ cc2.sh
@@ -7,12 +7,12 @@
 # Date of latest revision: may 31,2023     
 # Purpose:Create a bash script that:Copies /var/log/syslog to the current working directory and Appends the current date and time to the filename
 
-cp /var/log/syslog .
+#cp /var/log/syslog .
 
-echo "syslog copied to $(pwd)"
+#echo "syslog copied to $(pwd)"
 
 current_date=$(date +"%Y-%m-%d_%H-%M-%S")
-filename="syslog_${31may2023}"
+filename="syslog_$current_date"
 
 cp /var/log/syslog "${filename}"
 
