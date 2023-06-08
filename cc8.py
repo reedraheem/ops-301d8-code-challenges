a08a07178b80ad32300295d301a9698d15e2b3c5
reedraheem <Reedsharif@yahoo.com>
2023-06-08 19:40:26 +0000

    cc8

---
+++ cc8.py
@@ -0,0 +1,17 @@
+#Script: code challenge 8               
+# Author: Raheem Sharif Reed                     
+# Date of latest revision: June 7,2023     
+# Purpose:Create a Python script that includes the following:
+#Assign to a variable a list of ten string elements.
+#Print the fourth element of the list.
+#Print the sixth through tenth element of the list.
+#Change the value of the seventh element to “onion”.
+
+
+my_list = ["Roy jones Jr", "Floyd Mayweather Jr", "Pernell Whitaker", "Andre Ward", "Winky Wright", "Terrance Crawford", "Dmitry Bivol", "Shakur stevenson", "Thomas hearns", "James Toney"]
+
+print(my_list[4])
+print(my_list[6:])
+
+my_list[6] = "onion"
+print(my_list)
